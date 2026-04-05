0f626f1d5dfd331714cff8f1a5d8094d89e879b5
Md. Abdullah Al Mamun <mamun.renata52@gmail.com>
2026-04-05 20:18:37 +0600

    first commit

--- backend/database.py
+++ backend/database.py
@@ -1,17 +1,40 @@
+# import os
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import declarative_base
+# from sqlalchemy.orm import sessionmaker
+
+# # Use an environment variable with a default value.
+# # Inside Docker, DATABASE_URL will be provided to point to the host's database.
+# SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://root:mamun%401974@localhost/moon_light_medico")
+
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base = declarative_base()
+
+# def get_db():
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
+
+
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:mamun%401974@localhost/moon_light_medico"
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+# Create tables on startup
+Base.metadata.create_all(bind=engine)

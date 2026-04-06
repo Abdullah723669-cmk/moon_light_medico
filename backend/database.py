bb736384205e7a0a881a149e5469199d03dc487c
Md. Abdullah Al Mamun <mamun.renata52@gmail.com>
2026-04-06 16:16:43 +0600

    correction in database.py

--- backend/database.py
+++ backend/database.py
@@ -24,11 +24,11 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("moonlight_user")
+DB_PASSWORD = os.getenv("moonlight_password")
+DB_HOST = os.getenv("db")
+DB_PORT = os.getenv("3306")
+DB_NAME = os.getenv("moon_light_medico")
 
 DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
